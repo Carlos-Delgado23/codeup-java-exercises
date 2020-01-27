465acdf729b71b0ddb8b0cf33083dc3202246fcc
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-01-27 11:02:43 -0600

    syntax, types, and variables lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,36 @@ public class HelloWorld {
 //        System.out.print("Hello, ");
 //        System.out.print("World!");
 
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hellooo!";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
