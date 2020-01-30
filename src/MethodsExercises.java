1fa004ed397f70fee1ba73af514f5871d50efb09
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-01-30 10:24:41 -0600

    step three

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
 
 
 //       System.out.println(add(3, 5));
@@ -10,7 +11,16 @@ public class MethodsExercises {
 //       System.out.println(rMultiply(3,5));
 //       System.out.println(divide(3,5));
 
-       System.out.print(getInteger(1, 10));
+//       System.out.print(getInteger(1, 10));
+//        System.out.print(factorial(1, 10));
+
+        String confirm = "y";
+
+        while(confirm.equalsIgnoreCase("y")){
+            System.out.println(factorial(1, 10));
+            System.out.print("enter y to continue: ");
+            confirm = scanner.next();
+        }
     }
 
 
@@ -39,24 +49,41 @@ public class MethodsExercises {
 
 
 
-//    STEP TWO & THREE OF EXERCISES      //
+//    STEP TWO OF EXERCISE      //
     public static int getInteger(int min, int max){
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number between 1 and 10: ");
         int userInput = scanner.nextInt();
 
-        if (userInput > max){
-            System.out.print("Your number is greater than max: ");
-            return userInput;
-        } else if (userInput < min) {
-            System.out.print("Your number is less than min: ");
-            return userInput;
-        } else {
-            System.out.print("Your number is in range: ");
-            return userInput;
-        }
+            if (userInput > max){
+                System.out.print("Your number is greater than max: ");
+                return userInput;
+            } else if (userInput < min) {
+                System.out.print("Your number is less than min: ");
+                return userInput;
+            } else {
+                System.out.print("Your number is in range: ");
+                return userInput;
+            }
+    }
 
+//    STEP THREE OF EXERCISES      //
+    public static long factorial(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number between 1 and 10: ");
+         int userNum = scanner.nextInt();
+         long fact = 1;
+
+         if (userNum < max && userNum > min){
+             for (int i = 2; i <= userNum; i++) {
+                 fact = fact * i;
+             }
+             return fact;
+         } else {
+             return 0;
+         }
     }
 
 
