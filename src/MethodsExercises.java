a39698497fb3baa5a6fc87f264e7020d52149889
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-01-30 11:46:19 -0600

    step 4

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -10,17 +10,25 @@ public class MethodsExercises {
 //       System.out.println(multiply(3,5));
 //       System.out.println(rMultiply(3,5));
 //       System.out.println(divide(3,5));
+//       System.out.println(modulus(5, 3));
 
 //       System.out.print(getInteger(1, 10));
-//        System.out.print(factorial(1, 10));
+//        String intConfirm = "y";
+//        while(intConfirm.equalsIgnoreCase("y")){
+//            System.out.println(getInteger(1, 10));
+//            System.out.print("enter y to continue: ");
+//            intConfirm = scanner.next();
+//        }
 
-        String confirm = "y";
+//       System.out.print(factorial(1, 10));
+//        String confirm = "y";
+//        while(confirm.equalsIgnoreCase("y")){
+//            System.out.println(factorial(1, 10));
+//            System.out.print("enter y to continue: ");
+//            confirm = scanner.next();
+//        }
 
-        while(confirm.equalsIgnoreCase("y")){
-            System.out.println(factorial(1, 10));
-            System.out.print("enter y to continue: ");
-            confirm = scanner.next();
-        }
+        dice();
     }
 
 
@@ -47,23 +55,27 @@ public class MethodsExercises {
         return a / b;
     }
 
+    public static long modulus(int a, int b){
+        return a % b;
+    }
+
 
 
 //    STEP TWO OF EXERCISE      //
     public static int getInteger(int min, int max){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number between 1 and 10: ");
+        System.out.print("Enter a number between " + min + " and " + max + ": ");
         int userInput = scanner.nextInt();
 
             if (userInput > max){
-                System.out.print("Your number is greater than max: ");
+                System.out.println("Your number is greater than max: ");
                 return userInput;
             } else if (userInput < min) {
-                System.out.print("Your number is less than min: ");
+                System.out.println("Your number is less than min: ");
                 return userInput;
             } else {
-                System.out.print("Your number is in range: ");
+                System.out.println("Your number is in range: ");
                 return userInput;
             }
     }
@@ -72,20 +84,37 @@ public class MethodsExercises {
     public static long factorial(int min, int max){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number between 1 and 10: ");
+        System.out.print("Enter a number between" + min + "and" + max + ": ");
          int userNum = scanner.nextInt();
          long fact = 1;
 
          if (userNum < max && userNum > min){
              for (int i = 2; i <= userNum; i++) {
-                 fact = fact * i;
+                 fact *= i;
              }
              return fact;
          } else {
-             return 0;
+             System.out.print("Number isn't valid ");
+             return userNum;
          }
     }
 
-
+//    STEP FOUR OF EXERCISES      //
+    public static void dice (){
+        int dice1 = 0;
+        int dice2 = 0;
+        int numSides = 0;
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            System.out.println("How many sides per die (2 dice)? ");
+            numSides = getInteger(2, 21);
+            dice1 = (int) Math.round(Math.floor(Math.random() * numSides + 1));
+            dice2 = (int) Math.round(Math.floor(Math.random() * numSides + 1));
+            System.out.printf("%d + %d = %d\n", dice1, dice2, (int)add(dice1,dice2));
+            System.out.println("Would you like to roll again? ");
+            String answer = scan.next();
+            if (answer.toLowerCase().contains("n")) return;
+        }
+    }
 
 }
---
+++ src/TazTheUni.java
@@ -0,0 +1,10 @@
+public class TazTheUni {
+    public static void main(String[] args){
+
+
+
+
+    }
+
+
+}
